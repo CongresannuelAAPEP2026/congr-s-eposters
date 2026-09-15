5654d33d2611b684b801d500cb9a546e7b7f4d21
wail <congresannuelaapep2026@gmail.com>
2026-09-15 17:04:42 +0200

    Update placeholder names

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ function submitFormHTML(){
       <div class="row-2">
         <div class="field">
           <label for="auteur">Auteur (nom complet)</label>
-          <input type="text" id="auteur" name="auteur" placeholder="ex. Nadia BENSAID">
+          <input type="text" id="auteur" name="auteur" placeholder="ex. AHMED mohamed">
         </div>
         <div class="field">
           <label for="email">E-mail de l'auteur</label>
@@ -121,7 +121,7 @@ function submitFormHTML(){
       </div>
       <div class="field">
         <label for="coauteurs">Co-auteurs <span style="font-weight:400;color:var(--ink-soft);">(optionnel)</span></label>
-        <input type="text" id="coauteurs" name="coauteurs" placeholder="ex. K. AMRANI; S. BOUDJEMAA; R. HADDAD">
+        <input type="text" id="coauteurs" name="coauteurs" placeholder="ex. K. steve; S. jobs; R. jhon">
       </div>
       <div class="row-2">
         <div class="field">
